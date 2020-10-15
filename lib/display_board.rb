58cc976db385079aa27eb559626cb5f51ff13cd7
Vince W <vincewallgren@me.com>
2020-10-15 12:35:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -76,3 +76,13 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} |   |   "
 end
+
+
+board = ["X","O"," ","O","X","O"," ","X","O"]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} |   "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "   | #{board[7]} | #{board[8]} "
+end
